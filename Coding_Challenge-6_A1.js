a7f7f89edc2f824726c3c2de196cb15ea60999ab
dominoto <dominoto@users.noreply.github.com>
2022-02-07 21:59:22 +0100

    Update Coding_Challenge-6_A1.js

--- Coding_Challenge-6_A1.js
+++ Coding_Challenge-6_A1.js
@@ -1,29 +1,35 @@
 // Question 1: Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], make a function that organizes these into individual array that is ordered. For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
 
-let numbers = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
+let arrayFromAbove = [1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20];
 
-//Sorting the numbers
-numbers.sort((a, b) => a - b);
+//Function expression
+const answer = function (numbers) {
 
-//Counting duplicates
-const counts = {};
-numbers.forEach((x) => {
-    counts[x] = (counts[x] || 0) + 1;
-});
-console.log("Counted duplicates: ", counts)
+    //Sorting the numbers
+    numbers.sort((a, b) => a - b);
 
-//Creating new empty array
-let newArray = [];
+    //Counting duplicates
+    const counts = {};
+    numbers.forEach((x) => {
+        counts[x] = (counts[x] || 0) + 1;
+    });
+    console.log("Counted duplicates: ", counts);
 
-//Filling new array according to count
-for (let key in counts) {
-    if (counts[key] === 1) {
-        newArray.push(key);
-    } else {
-        let tempArray = new Array(counts[key]);
-        tempArray.fill(parseInt(key), 0, counts[key]);
-        newArray.push(tempArray);
+    //Creating new empty array
+    let newArray = [];
+
+    //Filling new array according to count
+    for (let key in counts) {
+        if (counts[key] === 1) {
+            newArray.push(key);
+        } else {
+            let tempArray = new Array(counts[key]);
+            tempArray.fill(parseInt(key), 0, counts[key]);
+            newArray.push(tempArray);
+        }
     }
+    console.log("Filling new array according to count: ", newArray);
 }
 
-console.log("Filling new array according to count: ", newArray)
+//Calling the function
+answer(arrayFromAbove);
